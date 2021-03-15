a720d161c82537c5a6d29cefb5044d8c0d53da97
Hector Menezes <hector.menezes@gmail.com>
2021-03-15 12:10:58 -0300

    Ativ5

---
+++ Atividade5/scriptMedia.js
@@ -0,0 +1,6 @@
+
+var nome = prompt("Insira o nome")
+var n1 = parseFloat(prompt("Insira a primeira nota"));
+var n2 = parseFloat(prompt("Insira a segunda nota"));
+var n3 = parseFloat(prompt("Insira a terceira nota"));    
+alert("O aluno " + nome + " teve a média " +(n1+n2+n3)/3 );
---
+++ Atividade5/scriptOperacoes.js
@@ -0,0 +1,3 @@
+var n1 = parseFloat(prompt("Insira o primeiro numero"));
+var n2 = parseFloat(prompt("Insira o segundo numero"));
+alert("Soma dos dois: " + (n1+n2) + "\nSubtracao do primeiro pelo segundo: " + (n1-n2) + "\nProduto: " + (n1*n2) + "\nDivisão do primero pelo segundo: " + (n1/n2) + "\nResto da divisão: " + (n1%n2));
