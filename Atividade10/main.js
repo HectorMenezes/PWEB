7234eac0462126d00a5ae1c78d82e3f25ec92086
Hector Menezes <hector.menezes@gmail.com>
2021-04-26 11:40:11 -0300

    Atividade 10

---
+++ Atividade10/main.js
@@ -0,0 +1,13 @@
+
+function abrejanela(obj){
+    document.getElementById("imagem").setAttribute("src", "./images/janelaaberta.png");
+
+}
+
+function fechajanela(obj){
+    document.getElementById("imagem").setAttribute("src", "./images/janelafechada.png");
+}
+
+function quebra(obj){
+    document.getElementById("imagem").setAttribute("src", "./images/janelaquebra.png")
+}
